7e69e0b1899aa1348364f35fafce310d861478bc
GreenyRepublic <benfclark@urog.co.uk>
2021-12-01 15:16:05 +0000

    Day 1 solution

---
+++ 1_SonarSweep.py
@@ -0,0 +1,38 @@
+import sys
+import io
+
+def CountDepthIncreases(depths: 'list[int]') -> int:
+    increases = 0
+    previous = 0
+    for depth in depths:
+        if depth > previous and previous != 0:
+            increases += 1
+        previous = depth
+        
+    return increases
+
+def CountDepthIncreasesWithWindows(depths: 'list[int]') -> int:
+    buckets = [0] * (len(depths) - 2)
+    for i in range(0,len(buckets)):
+        for j in range (0,3):
+            sample = depths[i + j]
+            buckets[i] += sample
+
+    return CountDepthIncreases(buckets)
+        
+
+def Main():
+
+    input = open("1_SonarSweep_input.txt")
+    data = input.readlines()
+    dataAsInts = list(map(int, data))
+
+    # Part 1
+    partOneAnswer = CountDepthIncreases(dataAsInts)
+    print(partOneAnswer)
+
+    # Part 2
+    partTwoAnswer = CountDepthIncreasesWithWindows(dataAsInts)
+    print(partTwoAnswer)
+
+Main()
