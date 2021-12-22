b0a6b2ebfa7478b0b03b5a3c6d7a9e1e0fdc4e60
Ben Clark <benfclark@urog.co.uk>
2021-12-22 03:10:48 +0000

    work on day 8

--- 12_Cave_Path.py
+++ 12_Cave_Path.py
@@ -4,20 +4,22 @@ from typing import Deque, Dict, List, Set
 inputFile = "12_Cave_Path_input.txt"
 inputTestFile = "12_Cave_Path_test_input.txt"
 
-def EnumerateRoutes(connections : Dict[str, List[str]], currentNode : str, currentPath : List[str] = [], currentVisited : Set[str]= set()) -> List[List[str]]:
+def EnumerateRoutes(connections : Dict[str, List[str]], currentNode : str, currentPath : List[str] = [], currentVisited : Set[str]= set(), canVisitTwice : bool = False, hasVisitedTwice : bool = False) -> List[List[str]]:
     
     currentPath.append(currentNode)
     if currentNode == "end":
         return [currentPath]
         
     if currentNode.islower():
+        if currentNode in currentVisited and currentNode != "start":
+            hasVisitedTwice = True
         currentVisited.add(currentNode)
     
     routes = []
 
     for node in connections[currentNode]:
-        if node not in currentVisited:
-            routes += (EnumerateRoutes(connections, node, list(currentPath), set(currentVisited)))
+        if (node not in currentVisited) or (canVisitTwice and not hasVisitedTwice) and node != "start":
+            routes += (EnumerateRoutes(connections, node, list(currentPath), set(currentVisited), canVisitTwice, hasVisitedTwice))
 
     return routes
     
@@ -43,11 +45,12 @@ def Main() -> None :
     caveNodes = ParseInput(inputFile)
 
     # Part 1
-    routes = EnumerateRoutes(caveNodes, "start")
-    print(str(len(routes)))
-
+    routesOne = EnumerateRoutes(caveNodes, "start", canVisitTwice=False)
+    print(str(len(routesOne)))
     
     # Part 2
+    routesTwo = EnumerateRoutes(connections=caveNodes, currentNode="start", canVisitTwice=True)
+    print(str(len(routesTwo)))
 
     
 Main()
--- 8_Seven_Segment.py
+++ 8_Seven_Segment.py
@@ -5,43 +5,85 @@ from typing import List, Tuple, DefaultDict
 inputFile = "8_Seven_segment_input.txt"
 
 DIGIT_SEGMENT_COUNTS = {
-    0 : 6,
-    1 : 2,
-    2 : 5,
-    3 : 5,
-    4 : 4,
-    5 : 5,
-    6 : 6,
-    7 : 3,
-    8 : 7,
-    9 : 6
+    2: [1],
+    3: [7],
+    4: [4],
+    5: [2,3,5],
+    6: [0, 6, 9],
+    7: [8]
 }
 
-UNIQUE_DIGITS = {
+UNIQUE_DIGIT_SIZES = {
     2 : 1,
     4 : 4,
     3 : 7,
     7 : 8
 }
 
+DISPLAY_SEGMENTS = {
+    "top",
+    "middle",
+    "bottom",
+    "left_up",
+    "left_down",
+    "right_up",
+    "right_down"
+}
+
+DIGIT_SEGMENT_MAPPINGS = {
+    0 : {"top", "bottom", "right_up", "right_down", "left_up", "left_down"},
+    1 : {"right_up", "right_down"},
+    2 : {"top", "right_up", "middle", "left_down", "bottom"},
+    3 : {"top", "right_up", "middle", "right_down", "bottom"},
+    4 : {"left_up", "middle", "right_up", "right_down"},
+    5 : {"top", "left_up", "middle", "right_down", "bottom"},
+    6 : {"top", "left_up", "middle", "left_down", "right_down", "bottom"},
+    7 : {"top", "right_up", "right_down"},
+    8 : {"top", "middle", "bottom", "left_up", "left_down", "right_up", "right_down"},
+    9 : {"top", "right_up", "left_up", "middle", "right_down", "bottom"},
+}
+
+
+
 class Signal:
     def __init__(self, inputCodes: str, displayedDigits: str):
         self._inputCodes = []            
         self._displayedDigits = []
+
         for incode in inputCodes.split():
             sortedCode = ''.join(sorted(incode))
             self._inputCodes.append(sortedCode)
+
         for digit in displayedDigits.split():
             sortedCode = ''.join(sorted(digit))
             self._displayedDigits.append(sortedCode)
 
-    def GetDigitCountInOutput(self, digit : int) -> int:
+        self._segments = {}
+        for key in DISPLAY_SEGMENTS:
+            self._segments[key] = {'a','b','c','d','g','e','f'}
+        
+
+    def GetUniqueDigitCountInOutput(self, digit : int) -> int:
         count = 0
         for num in self._displayedDigits:
-            if len(num) in UNIQUE_DIGITS.keys() and UNIQUE_DIGITS[len(num)] == digit:
+            if len(num) in UNIQUE_DIGIT_SIZES.keys() and UNIQUE_DIGIT_SIZES[len(num)] == digit:
                 count += 1
         return count
 
+    # Parse the mapping of input letters to their 'actual' segments
+    def ComputeMapping(self):
+        for code in self._inputCodes:
+            charSet = set()
+            for char in code: 
+                charSet.add(char)
+
+            possibleDigits = DIGIT_SEGMENT_COUNTS[len(code)]
+            for digit in possibleDigits:
+                for segment in DIGIT_SEGMENT_MAPPINGS[digit]:
+                    self._segments[segment].intersection_update(charSet)
+        
+        
+        print(self._segments)
 
 def ParseInput(filename : str) -> List[Signal]:    
     lines = open(filename).readlines()
@@ -60,9 +102,10 @@ def Main() -> None :
     occurences = 0
     for signal in signals:
         for num in digits:
-            occurences += signal.GetDigitCountInOutput(num)
+            occurences += signal.GetUniqueDigitCountInOutput(num)
     print(occurences)
     
-    # Part 2    
+    # Part 2
+    signals[0].ComputeMapping()
             
 Main()
