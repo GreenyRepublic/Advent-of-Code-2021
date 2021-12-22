22c3590b39ea5bad83f29f289929a89e23550828
Ben Clark <benfclark@urog.co.uk>
2021-12-22 20:27:27 +0000

    Day 14

--- 14_Polymers.py
+++ 14_Polymers.py
@@ -1,14 +1,43 @@
 from os import linesep, write
-from typing import Tuple, List, Dict
+from typing import Tuple, Dict
+from collections import defaultdict, Counter
+
 
 inputFile = "14_Polymers_input.txt"
 testInputFile = "14_Polymers_test_input.txt"
 
-def ParseInput(filename : str) -> Tuple[str, List[Dict[str, str]]]:  
+def ExpandPolymer(polymer : str, rules : Dict[str,str], steps : int):
+    charCount = defaultdict(int)
+    pairCount = defaultdict(int)
+
+    for char in polymer:
+        charCount[char] += 1
+    
+    for i  in range(0,len(polymer) - 1):
+        pair = polymer[i:i+2]
+        pairCount[pair] += 1
+
+    for i in range(0,steps):
+        newPairCount = defaultdict(int)
+        for rule in rules.keys():
+            count = pairCount[rule]
+
+            newChar = rules[rule]
+            charCount[newChar] += count
+            newPairA = rule[0] + newChar
+            newPairB = newChar + rule[1]
+            newPairCount[newPairA] += count
+            newPairCount[newPairB] += count
+            
+        pairCount = newPairCount
+
+    return charCount
+
+def ParseInput(filename : str) -> Tuple[str, Dict[str, str]]:  
 
     file = open(filename)
     startchain = ""
-    rules = []
+    rules = {}
     lineBreak = False
 
     for line in file.readlines():
@@ -18,24 +47,32 @@ def ParseInput(filename : str) -> Tuple[str, List[Dict[str, str]]]:
 
         if lineBreak:
             parsed = line.strip().split('->')
-            pattern = parsed[0]
-            insert = parsed[1]
-            rules.append({"pattern" : pattern, "insert" : insert})
+            pattern = parsed[0].strip()
+            insert = parsed[1].strip()
+            rules[pattern] = insert
 
         else:
-            startchain = line
+            startchain = line.strip()
         
 
     return startchain, rules
 
 def Main() -> None :
     startChain, rules = ParseInput(inputFile)
-    print(startChain)
-    print(rules)
-
-    # Part 1
     
+    # Part 1
+    firstPolymer = ExpandPolymer(startChain, rules, 10)
+    counts = Counter(firstPolymer).most_common()
+    largest = counts[0][1]
+    smallest  = counts[-1][1]
+    print(largest - smallest)
+
     # Part 2
+    secondPolymer = ExpandPolymer(startChain, rules, 40)
+    counts = Counter(secondPolymer).most_common()
+    largest = counts[0][1]
+    smallest  = counts[-1][1]
+    print(largest - smallest)
 
     
 Main()
